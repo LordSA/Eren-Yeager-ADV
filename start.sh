cfc382f7eaa38c59266ed97e8e8c690a6587c278
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-28 01:06:53 +0530

    229

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LordSA/Eren-Yeager-ADV5.2.0.git /Eren-Yeager-ADV5.2.0
+  git clone https://github.com/LordSA/Eren-Yeager-ADV.git /Eren-Yeager-ADV
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eren-Yeager-ADV5.2.0
+  git clone $UPSTREAM_REPO /Eren-Yeager-ADV
 fi
-cd /movie-world
+cd /Eren-Yeager-ADV
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
